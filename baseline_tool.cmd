8fc87430784be24aa3df1e0cb3bfe8908b07a74b
Doug <doug@defendthehoneypot.com>
2025-11-27 12:56:06 -0800

    Improve baseline tool robustness and flexibility

--- baseline_tool.cmd
+++ baseline_tool.cmd
@@ -6,61 +6,85 @@ REM by Doug Richmond (defendthehoneypot@gmail.com)
 REM
 REM About:
 REM Script to automate collecting a system baseline
-REM 
+REM
 REM Additional Tools Needed:
 REM Microsoft pslist.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/pslist
 REM Microsoft autorunssc.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/bb963902
 REM Microsoft sigcheck.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/bb897441
-REM 
+REM
 REM Folder Structure:
-REM		- Main Directory containing baseline_tool.cmd
-REM			- Tools: contains binaries for running with the script
+REM             - Main Directory containing baseline_tool.cmd
+REM                     - Tools: contains binaries for running with the script
 
 REM Setup:
 REM Set base folder, set input source file if needed, and make Results directory.
-SET scriptlocation=%~dp0
-SET src=%1
-SET dtstamp=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
-mkdir Results_%dtstamp%
+setlocal EnableExtensions EnableDelayedExpansion
+
+SET "scriptlocation=%~dp0"
+SET "src=%~1"
+IF "%src%"=="" SET "src=C:\"
+
+SET "hour=%time:~0,2%"
+IF "%hour:~0,1%"==" " SET "hour=0%hour:~1,1%"
+SET "dtstamp=%date:~-4%%date:~4,2%%date:~7,2%_%hour%-%time:~3,2%-%time:~6,2%"
+
+SET "resultsDir=%scriptlocation%Results_%dtstamp%"
+IF NOT EXIST "%resultsDir%" mkdir "%resultsDir%"
+
+SET "toolsDir=%scriptlocation%tools"
+SET "toolsList=pslist.exe autorunsc.exe sigcheck.exe"
+FOR %%T IN (%toolsList%) DO (
+    IF NOT EXIST "%toolsDir%\%%T" (
+        echo Required tool "%%T" not found in "%toolsDir%". Please download and place it in the Tools directory.
+        EXIT /B 1
+    )
+)
+
+echo Target drive: %src%
+echo Output folder: %resultsDir%
 
 echo.
 echo #######################################
-echo Collect list all files and folders on the C drive
+echo Collect list of all files and folders
 echo #######################################
 echo.
-dir /a /s /r /n /t C c:\  >> %scriptlocation%Results_%dtstamp%\baseline-files.txt
+dir /a /s /r /n /t:c %src% >> "%resultsDir%\baseline-files.txt"
 
 echo.
 echo #######################################
 echo Collect process list
 echo #######################################
 echo.
-%scriptlocation%\tools\pslist.exe -accepteula -nobanner -t >> %scriptlocation%Results_%dtstamp%\baseline-processlist.txt
+"%toolsDir%\pslist.exe" -accepteula -nobanner -t >> "%resultsDir%\baseline-processlist.txt"
 
 echo.
 echo #######################################
 echo Collect network information
 echo #######################################
 echo.
-netstat -nao >> %scriptlocation%Results_%dtstamp%\baselinenetstat.txt
+netstat -nao >> "%resultsDir%\baselinenetstat.txt"
 
 echo.
 echo #######################################
 echo Collect signature check from all files
 echo #######################################
 echo.
-%scriptlocation%\tools\sigcheck.exe -accepteula -nobanner -c -e -h -r -s c:\   >> %scriptlocation%Results_%dtstamp%\baseline-sigcheck.txt
+"%toolsDir%\sigcheck.exe" -accepteula -nobanner -c -e -h -r -s %src%   >> "%resultsDir%\baseline-sigcheck.txt"
 
 echo.
 echo #######################################
 echo Run autorunsc.exe to collect information about autostart locations
 echo #######################################
 echo.
-%scriptlocation%\tools\autorunsc.exe -accepteula -nobanner -a * -ct >> %scriptlocation%Results_%dtstamp%\baseline-autoruns.csv
+"%toolsDir%\autorunsc.exe" -accepteula -nobanner -a * -ct >> "%resultsDir%\baseline-autoruns.csv"
 
 echo.
 echo #######################################
 echo Collect all services
 echo #######################################
 echo.
-sc query >> %scriptlocation%Results_%dtstamp%\baseline-services.txt
+sc query >> "%resultsDir%\baseline-services.txt"
+
+echo.
+echo Baseline collection complete.
+endlocal
